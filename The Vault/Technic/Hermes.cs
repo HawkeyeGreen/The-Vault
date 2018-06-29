f2490b80d5c7659e08d3485b55896092c7ac963c
HawkeyeGreen <thymiana@web.de>
2018-06-29 14:33:02 +0200

    Arbeit an Hermes begonnen Blackboard fast fertig <3

---
+++ The Vault/Technic/Hermes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Vault.Technic
+{
+    /* Hermes - General Purpose Messaging System
+     * Hermes sammelt Zustandsmeldung etc. von beliebigen GameObjects und sammelt
+     * diese sowohl in einem geschriebenen Log als auch in einer Laufzeit-Sammlung verfügbar sind.
+     * 
+     * Darüber hinaus stehen das HermesBlackboard für die Kommunikation zwischen verschiedenen Programmteilen
+     * und das PlayerMessage-Log zu Verfügung.
+     * 
+     */
+    class Hermes
+    {
+        private static readonly Lazy<Hermes> lazy =
+        new Lazy<Hermes>(() => new Hermes());
+
+        private Dictionary<GameObject, List<Message>> blackboard = new Dictionary<GameObject, List<Message>>();
+
+        private Hermes()
+        {
+
+        }
+
+        public static Hermes Instance { get { return lazy.Value; } }
+
+        public void lodgeBlacboardMessage(GameObject sender, GameObject receiver, string message)
+        {
+            if(!blackboard.ContainsKey(receiver))
+            {
+                blackboard.Add(receiver, new List<Message>());
+            }
+            blackboard[receiver].Add(new Message(sender, message));
+        }
+
+        class Message
+        {
+            private GameObject sender;
+            public GameObject Sender { get => sender; set => sender = value; }
+
+            private string text;
+            public string Text { get => text; set => text = value; }
+
+            public Message(GameObject sender, string text)
+            {
+                this.Sender = sender;
+                this.Text = text;
+            }
+        }
+    }
+}
